8d9b2bd2f6c46f5618c68a5b07e2426d342cefe4
crayfish77 <40367244+crayfish77@users.noreply.github.com>
2019-11-03 15:57:31 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+//console.log("Up and running!");
+/*var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+console.log("User flipped " + cardOne);*/
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [] ;
+
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardOne);
+
+var cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+
+var cardThree = cards[2];
+cardsInPlay.push(cardThree);
+console.log("User flipped " + cardThree);
+
+var cardFour = cards[3];
+cardsInPlay.push(cardFour);
+console.log("User flipped " + cardFour);
+
+
+/*  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}*/
+
+var myArray = [
+{
+rank: "queen",
+suit: "hearts",
+casrImage: "images/queen-of-hearts.png"
+},
+{
+rank: "queen",
+suit: "diamonds",
+casrImage: "images/queen-of-diamonds.png"
+},
+{
+rank: "king",
+suit: "hearts",
+casrImage: "images/king-of-hearts.png"
+},
+{
+rank: "king",
+suit: "diamonds",
+casrImage: "images/king-of-diamonds.png"
+}
+];
